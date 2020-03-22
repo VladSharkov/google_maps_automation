f7a19c1d3b20f755ad3a229302746524eddf89df
vladimir.sharkov <Vladimir.Sharkov@careworkstech.com>
2020-03-21 20:35:03 -0400

    bussing working

--- map_automation/features/step_definitions/step_definitions.rb.rb
+++ map_automation/features/step_definitions/step_definitions.rb.rb
@@ -14,7 +14,7 @@ end
 Given(/^I am using (.*) directions$/) do |movement_type|
   case movement_type
   when 'driving'
-    on(MainNavigation).driving.click
+    on(MainNavigation).driving_element.click
   when 'public transport'
     on(MainNavigation).public_transport_element.click
   when 'biking'
@@ -27,63 +27,23 @@ Given(/^I am using (.*) directions$/) do |movement_type|
 end
 
 Then(/^I can see the direction details section$/) do
-  @browser.div(class: 'section-trip-details').present?
-  @browser.div(class: 'section-trip-summary noprint').text.include? 'Your destination is in a different time zone'
+  on(DirectionDetails).check_for_details
 end
 
 When(/^I click the details button$/) do
-  @browser.button('aria-labelledby' => 'section-directions-trip-details-msg-0').click
+  on(DirectionDetails).details
 end
 
 And(/^I can click on arrows to see more specifics$/) do
-  until @browser.div(class: 'directions-text-waypoint first-waypoint').text.include? 'Philadelphia'
-    sleep 0.1
-  end
-  number_of_arrows = @browser.buttons('aria-labelledby' => /directions-mode-group-title/).length
-  arrow_index = 0
-  while arrow_index < number_of_arrows
-    until @browser.div(id: /hideable_nontransit_0_#{arrow_index}/, style: /visibility: visible/).present?
-      @browser.button('aria-labelledby' => /directions-mode-group-title_0_#{arrow_index}/).click
-      sleep 0.5
-    end
-    arrow_index += 1
-  end
+  on(DirectionDetails).see_specifics
 end
 
 And(/^I can click the arrows to remove the specifics$/) do
-  number_of_arrows = @browser.buttons('aria-labelledby' => /directions-mode-group-title/).length
-  arrow_index = 0
-  while arrow_index < number_of_arrows
-    until @browser.div(id: /hideable_nontransit_0_#{arrow_index}/, style: /visibility: hidden/).exists?
-      @browser.button('aria-labelledby' => /directions-mode-group-title_0_#{arrow_index}/).click
-      sleep 0.5
-    end
-    arrow_index += 1
-  end
+  on(DirectionDetails).hide_specifics
 end
 
 When(/^I click through the details of various routes$/) do
-  until @browser.div(class: 'section-directions-trip-description').text.include? 'Philadelphia'
-    sleep 0.5
-  end
-  $number_of_routes = @browser.divs(id: /section-directions-trip-travel-mode-/).length
-  route_index = 0
-  $directions_array = []
-  while route_index < $number_of_routes
-    until @browser.div(class: 'transit-mode-body').present?
-      @browser.div(id: /section-directions-trip-travel-mode-#{route_index}/).click
-      # @browser.span(id: "section-directions-trip-details-msg-#{route_index}").click
-      # @browser.button('aria-labelledby' => /section-directions-trip-details-msg-#{route_index}/).click
-      sleep 0.5
-    end
-    $directions_array << @browser.div(class: 'transit-mode-body').text
-    @browser.button('aria-label' => ' Back ').click
-    sleep 3
-    route_index += 1
-    # 3.times {@browser.send_keys :tab}
-    @browser.div(id: "section-directions-trip-travel-mode-#{route_index}").click if route_index < $number_of_routes
-  end
-
+  on(DirectionDetails).check_bus_routes
 end
 
 Then(/^I will see different results$/) do
@@ -91,24 +51,32 @@ Then(/^I will see different results$/) do
 end
 
 Then(/^I will see direction details associated with (.*)$/) do |movement_type|
-  @browser.div(class: 'section-trip-details').present?
-  @browser.div(class: 'section-trip-summary noprint').text.include? 'Your destination is in a different time zone'
+  on(DirectionDetails).trip_details_element.present?
+  @current_page.trip_summary_element.text.include? 'Your destination is in a different time zone'
   case movement_type
   when 'walking'
-    @browser.div(xpath: '//*[@id="pane"]/div/div[1]/div/div/div[5]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[2]').text.include? 'walking directions may not always'
+    @current_page.walking_warnings_element.text.include? 'walking directions may not always'
   when 'biking'
-    @browser.div(xpath: '//*[@id="pane"]/div/div[1]/div/div/div[5]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[1]').text.include? 'bicycling directions may not always'
+    @current_page.biking_warnings_element.text.include? 'bicycling directions may not always'
   end
+  # @browser.div(class: 'section-trip-details').present?
+  # @browser.div(class: 'section-trip-summary noprint').text.include? 'Your destination is in a different time zone'
+  # case movement_type
+  # when 'walking'
+  #   @browser.div(xpath: '//*[@id="pane"]/div/div[1]/div/div/div[5]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[2]').text.include? 'walking directions may not always'
+  # when 'biking'
+  #   @browser.div(xpath: '//*[@id="pane"]/div/div[1]/div/div/div[5]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[1]').text.include? 'bicycling directions may not always'
+  # end
 end
 
 And(/^(.*) are a part of the initial directions$/) do |option|
   case option
   when 'highways'
-    expect(@browser.div(class: 'section-directions-trip-description').text.include? 'via I-').to eq true
+    expect(on(DirectionDetails).details_section_element.text.include? 'via I-').to eq true
   when 'tolls'
-    expect(@browser.div(class: 'section-directions-trip-description').text.include? 'This route has tolls').to eq true
+    expect(on(DirectionDetails).details_section_element.text.include? 'This route has tolls').to eq true
   when 'ferries'
-    expect(@browser.div(class: 'section-directions-trip-description').text.include? 'This route includes a ferry').to eq true
+    expect(on(DirectionDetails).details_section_element.text.include? 'This route includes a ferry').to eq true
   end
 end
 
@@ -128,11 +96,11 @@ end
 Then(/^the direction details will not include (.*)$/) do |option|
   case option
   when 'highways'
-    expect(@browser.div(class: 'section-directions-trip-description').text.include? 'via I-').to eq false
+    expect(on(DirectionDetails).details_section_element.text.include? 'via I-').to eq false
   when 'tolls'
-    expect(@browser.div(class: 'section-directions-trip-description').text.include? 'This route has tolls').to eq false
+    expect(on(DirectionDetails).details_section_element.text.include? 'This route has tolls').to eq false
   when 'ferries'
-    expect(@browser.div(class: 'section-directions-trip-description').text.include? 'This route includes a ferry').to eq false
+    expect(on(DirectionDetails).details_section_element.text.include? 'This route includes a ferry').to eq false
   end
 end
 
--- map_automation/features/support/pages/direction_details.rb
+++ map_automation/features/support/pages/direction_details.rb
@@ -1,5 +1,77 @@
 class DirectionDetails
 
+  include PageObject
 
+  button :details, {'aria-labelledby' => 'section-directions-trip-details-msg-0'}
+  div :trip_details, class: 'section-trip-details'
+  div :trip_summary, class: 'section-trip-summary noprint'
+  div :details_section, class: 'section-directions-trip-description'
+
+  def check_for_details
+    self.trip_details_element.present?
+    self.trip_summary_element.text.include? 'Your destination is in a different time zone'
+  end
+
+  #----------------------------------DRIVING SECTION-----------------------------------------
+
+  def see_specifics
+    until self.trip_details_element.text.include? 'Philadelphia'
+      sleep 0.1
+    end
+    number_of_arrows = @browser.buttons('aria-labelledby' => /directions-mode-group-title/).length
+    arrow_index = 0
+    while arrow_index < number_of_arrows
+      until @browser.div(id: /hideable_nontransit_0_#{arrow_index}/, style: /visibility: visible/).present?
+        @browser.button('aria-labelledby' => /directions-mode-group-title_0_#{arrow_index}/).click
+        sleep 0.5
+      end
+      arrow_index += 1
+    end
+  end
+
+  def hide_specifics
+    number_of_arrows = @browser.buttons('aria-labelledby' => /directions-mode-group-title/).length
+    arrow_index = 0
+    while arrow_index < number_of_arrows
+      until @browser.div(id: /hideable_nontransit_0_#{arrow_index}/, style: /visibility: hidden/).exists?
+        @browser.button('aria-labelledby' => /directions-mode-group-title_0_#{arrow_index}/).click
+        sleep 0.5
+      end
+      arrow_index += 1
+    end
+  end
+
+  #------------------------------------BUS SECTION-----------------------------------------
+
+  div :bus_details, class: 'section-directions-trip-description'
+
+  def check_bus_routes
+    until self.bus_details_element.present?
+      sleep 0.5
+    end
+    $number_of_routes = @browser.divs(id: /section-directions-trip-travel-mode-/).length
+    route_index = 0
+    $directions_array = []
+    while route_index < $number_of_routes
+      until @browser.div(class: 'transit-mode-body').present?
+        @browser.div(id: /section-directions-trip-travel-mode-#{route_index}/).click
+        sleep 0.5
+      end
+      $directions_array << @browser.div(class: 'transit-mode-body').text
+      @browser.button('aria-label' => ' Back ').click
+      sleep 3
+      route_index += 1
+      # 3.times {@browser.send_keys :tab}
+      @browser.div(id: "section-directions-trip-travel-mode-#{route_index}").click if route_index < $number_of_routes
+    end
+  end
+
+  #------------------------------------WALKING SECTION---------------------------------------
+
+  div :walking_warnings, {xpath: '//*[@id="pane"]/div/div[1]/div/div/div[5]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[2]'}
+
+  #------------------------------------BIKING SECTION----------------------------------------
+
+  div :biking_warnings, {xpath: '//*[@id="pane"]/div/div[1]/div/div/div[5]/div/div/div[1]/div/div[2]/div[2]/div[1]/div[1]'}
 
 end
