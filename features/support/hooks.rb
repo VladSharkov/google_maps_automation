2326096c5126d328b795ae5472958b6774ddd26e
Vlad Sharkov <vsharkov@gmail.com>
2020-03-22 22:16:23 -0400

    playing with reports

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,7 +8,7 @@ end
 After do |_scenario|
   if _scenario.failed?
     screenshot_filename = "#{_scenario.name.gsub(" ", "_")}_#{Time.now.to_s.gsub(" ", "_").gsub(":", "-")}"
-    @browser.screenshot.save "screenshots/#{screenshot_filename}.png"
+    @browser.screenshot.save "reports/screenshots/#{screenshot_filename}.png"
     embed 'screenshot.png', 'image/png'
   end
   @browser.close
